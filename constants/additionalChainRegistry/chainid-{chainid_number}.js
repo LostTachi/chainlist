d42e457cbd15f9bb10523915adcf8cbe405ec954
LostTachi <eugen_fatejew@gmx.de>
2025-09-17 10:01:11 +0200

    Create chainid-{chainid_number}.js

---
+++ constants/additionalChainRegistry/chainid-{chainid_number}.js
@@ -0,0 +1,24 @@
+{
+  "name": "Ronin Mainnet",
+  "chain": "RON",
+  "rpc": [
+    "https://api.roninchain.com/rpc",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Ronin",
+    "symbol": "RON",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "shortName": "ron",
+  "chainId": 2020,
+  "networkId": 1,
+  "icon": ronin",
+  "explorers": [{
+    "name": "blockexplorer",
+    "url": "https://app.roninchain.com/",
+    "icon": "ronin",
+    "standard": "EIP3091"
+  }]
+}
